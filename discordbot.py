141dd05276f0c2768d0f51995f3802bdf4007802
INAKINTV <52726516+INAKINTV@users.noreply.github.com>
2019-07-10 19:37:11 +0900

    reaction test

--- discordbot.py
+++ discordbot.py
@@ -7,6 +7,12 @@ bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
+@client.event
+async def on_reaction_add(reaction, user):
+print("reaction-by")
+
+
+
 @bot.event
 async def on_command_error(ctx, error):
     await ctx.send(str(error))
@@ -15,6 +21,7 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def 募集(ctx):
     await ctx.send('参加者を募集します')
+
 
 
 bot.run(token)
