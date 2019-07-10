c839798e6fbc5c0187035517d37c138a7132fac3
INAKINTV <52726516+INAKINTV@users.noreply.github.com>
2019-07-10 19:55:42 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -9,11 +9,6 @@ bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
-@client.event #ここから
-async def on_reaction_add(reaction, user):
-   # author: リアクションがついたメッセージを書いた人
-    author = reaction.message.author
-    await client.send_message(author, f"{user} さんがリアクションをしました")
 
 
 
