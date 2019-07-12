ac648f867e45f24463a4134c20d7564ca833a8ef
INAKINTV <52726516+INAKINTV@users.noreply.github.com>
2019-07-12 21:12:37 +0900

    反応

--- discordbot.py
+++ discordbot.py
@@ -15,6 +15,38 @@ async def on_command_error(ctx, error):
 async def ねこ(ctx):
     await ctx.send('にゃーん')
 
+    
+@bot.command()
+async def @5(ctx):
+    await ctx.send('@5だよ～')
+    
+    
+@bot.command()
+async def @4(ctx):
+    await ctx.send('@4だよ～')
+    
+    @bot.command()
+async def @3(ctx):
+    await ctx.send('@3だよ～')
+
+    @bot.command()
+async def @2(ctx):
+    await ctx.send('@2だよ～')
+
+@bot.command()
+async def @1(ctx):
+    await ctx.send('@1だよ～')
+
+@bot.command()
+async def 〆(ctx):
+    await ctx.send('〆だよ～')
+
+
+
+
+
+
+
 
 bot.run(token)
 
